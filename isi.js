88e01569312e8abbf40252e62aba4b677322244c
FT2313025009 <fajartriadi224@gmail.com>
2024-10-02 14:02:55 +0700

    Update isi.js

--- isi.js
+++ isi.js
@@ -1,7 +1,7 @@
-document.writeln("<center>");
+document.writeln ("<center>");
 document.writeln ("<hr width=600 color=black>");
 document.writeln ("<h2> UNIVERSITAS LAMPUNG </h2>");
 document.writeln ("<h2> PENDIDIKAN TEKNOLOGI INFORMASI </H2>);
 document.writeln ("<h3> JURUSAN PENDIDIKAN MATEMATIKA DAN PENGETAHUAN ALAM </h3>");
-document.writeln("<hr width=600 color=Blacl>");
+document.writeln ("<hr width=600 color=Blacl>");
 document.writeln ("</center>")
